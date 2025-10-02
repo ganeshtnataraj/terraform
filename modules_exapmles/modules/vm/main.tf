fae6853377fd1d71c08300f856ba898cce7e0d3d
ganeshtnataraj <ganeshtnataraj.91@gmail.com>
2025-10-02 12:30:49 +0530

    moved to a new dir

---
+++ modules_exapmles/modules/vm/main.tf
@@ -0,0 +1,69 @@
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vm-public-ip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-demo"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "vm-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "ubuntu-vm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = "Standard_DS1_v2"
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  disable_password_authentication = false   # ✅ Allow password login
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    name                 = "vm-os-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+}
